c7eaa77d7a6a97789a2bf59a4bd77784bf5014aa
Your Name <you@example.com>
2015-12-23 03:17:15 +0300

    изменено:      etc/ace.bat

--- etc/ace.bat
+++ etc/ace.bat
@@ -1,4 +1,5 @@
 @echo off
+set id=856f4dfedc733c085ff24ca6908fad583ba4e477
 c:
 cd C:\Documents and Settings\vcore\Application Data\ACEStream\player\
-start ace_player.exe    acestream://856f4dfedc733c085ff24ca6908fad583ba4e477  --sout="#http{mux=ts,dst=:8081/}" --sout-keep
+start ace_player.exe    acestream://%id%  --sout="#http{mux=ts,dst=:8081/}" --sout-keep
