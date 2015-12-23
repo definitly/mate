832562ff5f0d49675ffe8c4978f809571be8d4ff
Your Name <you@example.com>
2015-12-23 08:36:09 +0300

    новый файл:    userfolder/scripts/swfplayer.sh

---
+++ userfolder/scripts/swfplayer.sh
@@ -0,0 +1,61 @@
+#!/usr/local/bin/bash
+
+sudo kldload ipfw
+FwCMD="/sbin/ipfw"
+
+sudo ${FwCMD} -q  -f flush
+sudo ${FwCMD} add allow ip from any to any via lo0
+sudo ${FwCMD} add deny ip from any to 127.0.0.0/8
+sudo ${FwCMD} add deny ip from 127.0.0.0/8 to any
+
+sudo ${FwCMD} add 40 fwd 127.0.0.1, 1935 tcp from any to any 1935
+sudo ${FwCMD} add 40 fwd 127.0.0.1, 1935 tcp from any to any 1935 not uid proxy
+#sudo ${FwCMD} add 40 fwd 127.0.0.1, 1935 tcp from any to any 443
+#sudo ${FwCMD} add 40 fwd 127.0.0.1, 1935 tcp from any to any 443 not uid proxy
+sudo ${FwCMD} add allow ip from any to any via ale0
+
+                     rtmpsrv >  /tmp/rtmp1  &  
+               sudo  tcpdump -w file.tcpdump -s 0 -i ale0 &      
+
+
+while  ! [  "$rt" -o  "$rt2" ]  ; do
+
+ rt=$(tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri"  | grep m3u8)
+ rt2=$(cat /tmp/rtmp1 | grep rtmp://)
+ 
+ sleep 1;
+
+done 
+
+     killall -9 rtmpsrv
+sudo killall -9 tcpdump
+
+array=( m3u8 m3u8? )
+
+for i in "${array[@]}"
+do
+	
+
+ host=$(tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri" | grep $i | head -n 1 | awk ' {print $1} ')
+     GET=$(tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri" | grep $i | head -n 1 | awk ' {print $3} ')
+     url=http://$host$GET
+echo $host
+echo $GET
+echo $url 
+
+mpv $url  > /dev/null 2>&1
+
+done
+
+
+
+sudo  kldunload ipfw
+sed -r 's/-o.+//' /tmp/rtmp1 >  /tmp/rtmp2
+rtmp=$(head -n 2 /tmp/rtmp2 | tail -n 1 | sed  's/$/ | mpv --cache=2048  -   /')
+echo $rtmp'> /dev/null'
+sudo rm *.flv
+echo '#!/bin/sh' >> runrtmp.sh
+echo $rtmp'> /dev/null 2>&1'     >>  runrtmp.sh
+chmod +x runrtmp.sh
+./runrtmp.sh
+rm runrtmp.sh
