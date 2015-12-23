7a0d8a0718ec9abe73fa10d1b5b6f84e5b1f3dfe
Your Name <you@example.com>
2015-12-23 03:02:32 +0300

    новый файл:    etc/ace.bat

---
+++ etc/ace.bat
@@ -0,0 +1,2 @@
+@echo off
+start "C:\Documents and Settings\vcore\Application Data\ACEStream\player\ace_player.exe    acestream://856f4dfedc733c085ff24ca6908fad583ba4e477"  --sout="#http{mux=ts,dst=:8081/}" --sout-keep
