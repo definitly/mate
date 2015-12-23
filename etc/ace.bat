b90f433391a6c5d113d2196b91708cb1f1f490c6
Your Name <you@example.com>
2015-12-24 02:26:59 +0300

    изменено:      etc/ace.bat

--- etc/ace.bat
+++ etc/ace.bat
@@ -3,5 +3,5 @@ chcp 1251 >nul
 set id=a9fc9e195b3032d13b5e1380cdbc01c0de9ec1f9
 c:
 
-cd C:\Documents and Settings\Администратор\Application Data\ACEStream\player
+cd C:\Documents and Settings\Admin\Application Data\ACEStream\player
 start ace_player.exe    acestream://%id%  --sout="#http{mux=ts,dst=:8081/}" --sout-keep
