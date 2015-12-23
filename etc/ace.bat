73d99af738b1e48a1122051203f46382c71060f4
Your Name <you@example.com>
2015-12-23 03:14:31 +0300

    изменено:      etc/ace.bat

--- etc/ace.bat
+++ etc/ace.bat
@@ -1,2 +1,4 @@
 @echo off
-start "C:\Documents and Settings\vcore\Application Data\ACEStream\player\ace_player.exe    acestream://856f4dfedc733c085ff24ca6908fad583ba4e477"  --sout="#http{mux=ts,dst=:8081/}" --sout-keep
+c:
+cd C:\Documents and Settings\vcore\Application Data\ACEStream\player\
+start ace_player.exe    acestream://856f4dfedc733c085ff24ca6908fad583ba4e477  --sout="#http{mux=ts,dst=:8081/}" --sout-keep
