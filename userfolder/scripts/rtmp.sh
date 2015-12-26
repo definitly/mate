d408cdbcf8d703b2e8dd6e7e6d3a1c8ecbbf3d45
Your Name <you@example.com>
2015-12-27 01:44:56 +0300

    изменено:      userfolder/scripts/rtmp.sh 	изменено:      userfolder/scripts/swfplayer.sh

--- userfolder/scripts/rtmp.sh
+++ userfolder/scripts/rtmp.sh
@@ -19,7 +19,7 @@ sudo ${FwCMD} add 40 fwd 127.0.0.1, 1935 tcp from any to any 1935 not uid proxy
 #sudo ${FwCMD} add 40 fwd 127.0.0.1, 1935 tcp from any to any 443 not uid proxy
 sudo ${FwCMD} add allow ip from any to any via ale0
 
- rtmpsrv >  /tmp/rtmp1  &  
+ rtmpsrv >  /tmp/rtmp1  2>&0  &
 
 
 while   [ -z "$rt" ]  ; do
