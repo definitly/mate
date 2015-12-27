04da560fe3b1e8a3d29f458df66593d22d65821a
Your Name <you@example.com>
2015-12-27 06:34:32 +0300

    изменено:      userfolder/scripts/swfplayer.sh

--- userfolder/scripts/swfplayer.sh
+++ userfolder/scripts/swfplayer.sh
@@ -35,22 +35,18 @@ sudo killall -9 tcpdump
 if   ! [   -z "$m3u8"] ; then
 
 
-array=( m3u8 m3u8? )
 
-for i in "${array[@]}"
-do
-	
 
  host=$(tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri" | grep $i | head -n 1 | awk ' {print $1} ')
      GET=$(tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri" | grep $i | head -n 1 | awk ' {print $3} ')
      url=http://$host$GET
-echo $host
-echo $GET
+
+
 echo $url 
 
 mpv $url  > /dev/null 2>&1
 
-done
+
 
 fi
 
