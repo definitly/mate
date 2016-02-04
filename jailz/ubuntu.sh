10323a47e9ebfe770c73d43d4cce5f25c26a43d4
Your Name <you@example.com>
2016-02-04 09:25:39 +0300

    изменено:      jailz/ubuntu.sh

--- jailz/ubuntu.sh
+++ jailz/ubuntu.sh
@@ -4,7 +4,7 @@ chown -R man:root /var/cache/man
 apt-get update && apt-get -y upgrade
 apt-get -y install libx11-6  libxext6 libXtst6 libXfixes3 libXrandr2 libgtk2.0-0  libopenal1 libnm-glib4  libusb-1.0-0  libsm6 libasound2 libgconf2-4 dbus libsndfile1 \
  libnss3 libdbus-glib-1-2 libstdc++5 libcurl3 libxt6 libcrystalhd3  libjson0  libspeexdsp1   \
-libXv1 libXss1 libqt4-dbus mc libqt4-webkit libxxf86vm1  libx11-xcb1
+libXv1 libXss1  libxxf86vm1  libx11-xcb1
 
 
 
