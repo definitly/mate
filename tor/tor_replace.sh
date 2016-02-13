4ce704fb9509bd2bf7706d583a494e1e63328cac
Your Name <you@example.com>
2016-02-13 05:36:35 +0300

    изменено:      tor/tor_replace.sh 	новый файл:    userfolder/.mutt/certificates

--- tor/tor_replace.sh
+++ tor/tor_replace.sh
@@ -17,17 +17,17 @@ fi
 
 
 
-sed  's/SocksPort 192.168.1.38:9050 # Bind to this address:port too./SocksPort 192.168.1.'"$1"':9050 # Bind to this address:port too./g' /usr/local/etc/tor/torrc >  /usr/local/etc/tor/torrc.bak 
+sed  's/SocksPort 192.168.1.35:9050 # Bind to this address:port too./SocksPort 192.168.1.'"$1"':9050 # Bind to this address:port too./g' /usr/local/etc/tor/torrc >  /usr/local/etc/tor/torrc.bak 
 
 mv /usr/local/etc/tor/torrc.bak /usr/local/etc/tor/torrc
 
 
-sed  's/proxyAddress = "192.168.1.38"    # IPv4 only/proxyAddress = "192.168.1.'"$1"'"    # IPv4 only/g'    /usr/local/etc/polipo/config > /usr/local/etc/polipo/config.bak
+sed  's/proxyAddress = "192.168.1.35"    # IPv4 only/proxyAddress = "192.168.1.'"$1"'"    # IPv4 only/g'    /usr/local/etc/polipo/config > /usr/local/etc/polipo/config.bak
 
 mv /usr/local/etc/polipo/config.bak /usr/local/etc/polipo/config  
 
 
-sed 's/socksParentProxy = "192.168.1.38:9050"/socksParentProxy = "192.168.1.'"$1"':9050"/g' /usr/local/etc/polipo/config >  /usr/local/etc/polipo/config.bak
+sed 's/socksParentProxy = "192.168.1.35:9050"/socksParentProxy = "192.168.1.'"$1"':9050"/g' /usr/local/etc/polipo/config >  /usr/local/etc/polipo/config.bak
 
 mv /usr/local/etc/polipo/config.bak  /usr/local/etc/polipo/config 
 
