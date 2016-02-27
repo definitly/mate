77aa0fb60d6a455bddef80330bdff5339495d36a
Your Name <you@example.com>
2016-02-27 23:34:28 +0300

    изменено:      userfolder/scripts/install_scripts/winesteam_install.sh 	удалено:       userfolder/scripts/win.sh

--- userfolder/scripts/install_scripts/winesteam_install.sh
+++ userfolder/scripts/install_scripts/winesteam_install.sh
@@ -1,32 +1,26 @@
 #!/bin/sh
 login=definitly
 
-
-MAC=`ifconfig -a | grep ether | awk '{print $2}'| sed s/://g | sed 's/.\{1\}/& /g'`
-echo $MAC
-a=`echo $MAC | awk '{print $9}'`
-b=`echo $MAC | awk '{print $6}'`
-c=`echo $MAC | awk '{print $3}'`
-d=$(($c+4))
-e=`echo $MAC | awk '{print $11}'`
-f=$(($e-4))
-g=`echo $MAC | awk '{print $1}'`
-h=`echo $MAC | awk '{print $7}'`
-i=`echo $MAC | awk '{print $2}'`
-j=$(($i+1))
-k=`echo $MAC | awk '{print $5}'`
-l=$a$b$d$f$g$h$j$k
-echo $l
+if [ -z "$1" ]
+then
+  echo "Значение не задано"
+  echo "введите пароль"
+  echo " пример: ./winesteam_install.sh password"
+  exit 1
+fi
 
 
-fetch  https://dl.dropboxusercontent.com/u/$l/winesteam/winesteam
+    gdrive download -i 0Bz5rjpkCwREsNDh4NHltZF9SUkE
 
+
+
+  echo $1 > /tmp/winegeli
 
 sudo mdconfig -at vnode -f winesteam 
-sudo geli attach   /dev/md0
+sudo geli attach  -j /tmp/winegeli   /dev/md0
 sudo mount /dev/md0.eli /mnt
 
-tar xpvf /mnt/winesteam.tar.xz  -C  ~/
+tar xf /mnt/wine.txz  -C  ~/
 sudo umount /dev/md0.eli
 sudo geli detach md0.eli
 sudo mdconfig -d -u 0 
