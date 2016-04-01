9b3a7e13007ba97d4299a4474fb5e4bdffad83d9
Your Name <you@example.com>
2016-04-01 05:26:31 +0300

    изменено:      jailz/hl.sh

--- jailz/hl.sh
+++ jailz/hl.sh
@@ -8,33 +8,17 @@
 
 GAMEROOT="/usr/home/definitly/.local/share/Steam/steamapps/common/Half-Life"
 
-#determine platform
-UNAME=`uname`
-if [ "$UNAME" == "Darwin" ]; then
-   # prepend our lib path to LD_LIBRARY_PATH
-   export DYLD_LIBRARY_PATH=${GAMEROOT}:$DYLD_LIBRARY_PATH
-elif [ "$UNAME" == "FreeBSD" ]; then
-   # prepend our lib path to LD_LIBRARY_PATH
-   export LD_LIBRARY_PATH=${GAMEROOT}:$LD_LIBRARY_PATH
-fi
-
-if [ -z $GAMEEXE ]; then
-	if [ "$UNAME" == "Darwin" ]; then
-	  GAMEEXE=hl_osx
-	elif [ "$UNAME" == "FreeBSD" ]; then
-	  GAMEEXE=hl_linux
-	fi
-fi
-
-
+ sudo   mv /compat/linux/usr/lib/libpulse.so.0  /compat/linux/usr/lib/libpulse.so.0.bak
 
 # and launch the game
 cd "$GAMEROOT"
 
-STATUS=42
-while [ $STATUS -eq 42 ]; do
+
           export LD_LIBRARY_PATH=${GAMEROOT}:$LD_LIBRARY_PATH
-	 /usr/home/definitly/.local/share/Steam/steamapps/common/Half-Life/hl_linux $@
+	 /usr/home/definitly/.local/share/Steam/steamapps/common/Half-Life/hl_linux -steam -game cstrike
 	
-done
-exit $STATUS
+
+
+
+sudo mv /compat/linux/usr/lib/libpulse.so.0.bak   /compat/linux/usr/lib/libpulse.so.0
+
