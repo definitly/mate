a755bef6e238b0ee273d5bb7868eaddf1472c914
Your Name <you@example.com>
2016-05-05 10:57:33 +0300

    изменено:      jailz/installsteam.sh

--- jailz/installsteam.sh
+++ jailz/installsteam.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+
+ if ! [ -d /compat/ubuntu ]; then
+
+     gdrive download -i 0Bz5rjpkCwREsOGJzbUlmUVl2UGM
+
+     sudo     tar -xf ubuntu12.04.tar.gz -C /compat
+   
+   fi 
+
+      
+
+
+
 fetch http://repo.steampowered.com/steam/archive/precise/steam_latest.tar.gz
   
 
