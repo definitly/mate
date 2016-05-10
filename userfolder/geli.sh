4eb64b93f6557d1527dd891792b924dc1cb004b2
Your Name <you@example.com>
2016-05-10 04:45:48 +0300

    изменено:      userfolder/geli.sh

--- userfolder/geli.sh
+++ userfolder/geli.sh
@@ -42,7 +42,8 @@ echo $k
 
    if ! [ -f  freebsd ]; then
                   
-   fetch  https://dl.dropboxusercontent.com/u/$l/$k
+   #fetch  https://dl.dropboxusercontent.com/u/$l/$k
+   fetch "https://drive.google.com/uc?id=0Bz5rjpkCwREsQkY3elhkbW4ycjg"
    
 
    fi
