8ea5c74965d4d1a5b2a8de28026f8ca3522ff154
Your Name <you@example.com>
2016-05-13 01:28:00 +0300

    изменено:      userfolder/scripts/swfplayer.sh

--- userfolder/scripts/swfplayer.sh
+++ userfolder/scripts/swfplayer.sh
@@ -37,8 +37,8 @@ if   ! [   -z "$m3u8" ] ; then
 
 
 
- host=$(tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri" | grep m3u8 | head -n 1 | awk ' {print $1} ')
-     GET=$(tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri" | grep m3u8 | head -n 1 | awk ' {print $3} ')
+ host=$(tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri" | grep m3u8 | head -n 1 | tail -1 | awk ' {print $1} ')
+     GET=$(tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri" | grep m3u8 | head -n 1 | tail -1 | awk ' {print $3} ')
      url=http://$host$GET
 
 
