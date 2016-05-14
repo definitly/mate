8538e77ebdd7e948104e6ed485f06f110de293b9
Your Name <you@example.com>
2016-05-14 10:04:12 +0300

    удалено:       userfolder/scripts/swf/m3u8.sh 	изменено:      userfolder/scripts/swf/m3u8player.sh

--- userfolder/scripts/swf/m3u8player.sh
+++ userfolder/scripts/swf/m3u8player.sh
@@ -3,32 +3,29 @@
  sudo tcpdump -w file.tcpdump -s 0 -i ale0 &
 
 
-while   [ -z "$rt" ]  ; do
+              while   [ -z "$rt" ]  ; do
 
- rt=$( tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri"  | grep m3u8  )
+                        rt=$( tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri"  | grep m3u8  )
+                        sleep 1;
 
- sleep 1;
+              done 
 
-done 
+                 sudo killall -9 tcpdump
 
-sudo killall -9 tcpdump
+               array=( m3u8 m3u8? )
 
-array=( m3u8 m3u8? )
-
-for i in "${array[@]}"
-do
-	
-
- host=$(tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri" | grep $i | head -n 1 | awk ' {print $1} ')
-     GET=$(tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri" | grep $i | head -n 1 | awk ' {print $3} ')
-     url=http://$host$GET
+              for i in "${array[@]}"
+     do
+            host=$(tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri" | grep $i | head -n 1 | awk ' {print $1} ')
+            GET=$(tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri" | grep $i | head -n 1 | awk ' {print $3} ')
+            url=http://$host$GET
 echo $host
 echo $GET
 echo $url 
 
-mpv $url  > /dev/null 2>&1
+            mpv $url  > /dev/null 2>&1
 
-done
+     done
 
 
 
