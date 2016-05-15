7b86533db1989b5a3b57e5300377d4ae020375cf
Your Name <you@example.com>
2016-05-15 07:24:09 +0300

    изменено:      userfolder/scripts/tcpdump_m3u8.sh

--- userfolder/scripts/tcpdump_m3u8.sh
+++ userfolder/scripts/tcpdump_m3u8.sh
@@ -1,31 +1,41 @@
 #!/usr/local/bin/bash
 
-sudo tcpdump -w file.tcpdump -s 0 -i ale0 &
 
+sudo rm file.tcpdump
+sudo tcpdump -w file.tcpdump -s 0 -i ale0 &
 
               while   [ -z "$rt" ]  ; do
 
-                        rt=$(  tcpdump -nnr file.tcpdump |  grep m3u8  )
+                        rt=$(  tcpdump  -A -nnr file.tcpdump |  grep m3u8  )
                         sleep 1;
 
               done 
 
 sudo killall -9 tcpdump
 
+     host=$( tcpdump -A -nnr file.tcpdump '(((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)'  |  egrep --line-buffered "^........(GET |HTTP\/|POST |HEAD )|^[A-Za-z0-9-]+: " | sed -r 's/^........(GET |HTTP\/|POST |HEAD )/\n\1/g' | grep Host | tail -1 | awk ' {print $2} ' )
 
       array=( m3u8 m3u8? )
 
               for i in "${array[@]}"
       do
-            host=$(tcpdump -nnr file.tcpdump | grep $i | awk ' {print $5} ' | sed 's/.$//'   | rev | sed -e 's/\([0-9]\)\./\1:/' | rev  )
-            GET=$(tcpdump -nnr file.tcpdump  | grep $i | awk ' {print $24} ')
-            url=http://$host$GET
+#            host=$(tcpdump -nnr file.tcpdump | grep $i | awk ' {print $5} ' | sed 's/.$//'   | rev | sed -e 's/\([0-9]\)\./\1:/' | rev  )
+             GET=$(tcpdump -nnr file.tcpdump  | grep $i | awk ' {print $24} ')
+  
+    if [ -z $GET ]
+ then   
+        GET=$(tcpdump -A  -nnr file.tcpdump  | grep $i | awk ' {print $2} ')
+            
+    fi
+ 
+url=http://$host$GET
 echo $host
 echo $GET
 echo $url 
 
+mpv $url  > /dev/null 2>&1
+
      done
 
 
-mpv $url  > /dev/null 2>&1
-sudo rm file.tcpdump
+
