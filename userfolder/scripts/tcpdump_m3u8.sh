855e3882ba9b63c2e571c7fffe008bf46e0b3673
Your Name <you@example.com>
2016-05-16 08:33:17 +0300

    изменено:      userfolder/scripts/tcpdump_m3u8.sh

--- userfolder/scripts/tcpdump_m3u8.sh
+++ userfolder/scripts/tcpdump_m3u8.sh
@@ -14,9 +14,9 @@ sudo tcpdump -w file.tcpdump -s 0 -i ale0 &
 sudo killall -9 tcpdump
 
 
-#all all aviable hosts 
-
-
+# all aviable hosts 
+      echo "######################################################################################################"
+      echo "All aviable host"
      tcpdump -A -nnr file.tcpdump '(((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)'   2>&1 \
      | egrep --line-buffered "^........(GET |HTTP\/|POST \  |HEAD )|^[A-Za-z0-9-]+: "\
      | sed -r 's/^........(GET |HTTP\/|POST |HEAD )/\n\1/g' | grep Host |  awk '{print $2}'  
@@ -26,7 +26,7 @@ sudo killall -9 tcpdump
 
 #############################################################################################################################################################
      
-     host=$(tcpdump -A -nnr file.tcpdump '(((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)' \
+     host=$(tcpdump -A -nnr file.tcpdump '(((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)'  2>&1  \
      | egrep --line-buffered "^........(GET |HTTP\/|POST \  |HEAD )|^[A-Za-z0-9-]+: " \
      | sed -r 's/^........(GET |HTTP\/|POST |HEAD )/\n\1/g' | grep Host | awk '{print $2}'\
      | sed -r '/^[a-z]/d' | head -n 1)
@@ -36,11 +36,11 @@ sudo killall -9 tcpdump
               for i in "${array[@]}" ;do
 
 #     host=$(tcpdump -nnr file.tcpdump | grep $i | awk ' {print $5} ' | sed 's/.$//'   | rev | sed -e 's/\([0-9]\)\./\1:/' | rev  )
-      GET=$(tcpdump -nnr file.tcpdump  | grep $i | awk ' {print $24} ')
+      GET=$(tcpdump -nnr file.tcpdump   2>&1 | grep $i | awk ' {print $24} ')
   
                   if [ -z "$GET" ] ;then   
         
-             GET=$(tcpdump -A  -nnr file.tcpdump  | grep $i | awk ' {print $2} ')
+             GET=$(tcpdump -A  -nnr file.tcpdump  2>&1  | grep $i | awk ' {print $2} ')
             
                   fi
  
