3b9534b6ba125a8861d9dc8c763ecbb889da5e44
Your Name <you@example.com>
2016-05-17 06:08:20 +0300

    изменено:      micro_httpd/install.sh

--- micro_httpd/install.sh
+++ micro_httpd/install.sh
@@ -2,6 +2,4 @@
 
 echo 'inetd_enable=YES'                                >>            /etc/rc.conf
 echo 'micro_httpd     8000/tcp   #micro_httpd'         >>            /etc/services
-echo 'micro_httpd      stream  tcp \
-      nowait  nobody  /usr/local/sbin/micro_httpd\
-      micro_httpd            /usr/local/www/nginx'     >>            /etc/inetd.conf
+echo 'micro_httpd      stream  tcp nowait  nobody  /usr/local/sbin/micro_httpd    micro_httpd            /usr/local/www/nginx'     >>            /etc/inetd.conf
