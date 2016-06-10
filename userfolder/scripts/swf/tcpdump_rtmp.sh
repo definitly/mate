6eedb5fd4c1f03930d851fdbc2cf2a7b36cc22f3
Your Name <you@example.com>
2016-06-10 23:11:45 +0300

    удалено:       userfolder/scripts/swf/old/play_tcUrl 	переименовано: userfolder/scripts/swf/old/sawlive.tv -> userfolder/scripts/swf/sawlive.tv 	удалено:       userfolder/scripts/swf/tcpdump_rtmp.sh

--- userfolder/scripts/swf/tcpdump_rtmp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-
-sudo tcpdump -w file.tcpdump -s 0 -i ale0
-tcpdump -A -nnr file.tcpdump  2>&1   |   grep -ao "play.\+.==" | sed 's|.*B||' > old/play_tcUrl
-tcpdump -A -nnr file.tcpdump  2>&1   |   grep -ao "rtmp://.\+.flash" |  sed 's/\.//' >> old/play_tcUrl
-
