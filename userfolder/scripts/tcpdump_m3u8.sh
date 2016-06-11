49489bf5135bdc16fded733bd359125f05721c68
Your Name <you@example.com>
2016-06-11 07:36:00 +0300

    изменено:      userfolder/scripts/swf/sawlive.tv 	удалено:       userfolder/scripts/swfplayer.sh 	изменено:      userfolder/scripts/tcpdump_m3u8.sh

--- userfolder/scripts/tcpdump_m3u8.sh
+++ userfolder/scripts/tcpdump_m3u8.sh
@@ -1,12 +1,12 @@
 #!/usr/local/bin/bash
 
-
-sudo rm file.tcpdump
-sudo tcpdump -w file.tcpdump -s 0 -i ale0 &
+file.tcpdump=/tmp/1.tcpdump
+sudo rm $file.tcpdump
+sudo tcpdump -w $file.tcpdump -s 0 -i ale0 &
 
               while   [ -z "$rt" ]  ; do
 
-                        rt=$(  tcpdump  -A -nnr file.tcpdump 2>&1 |  grep m3u8  )
+                        rt=$(  tcpdump  -A -nnr $file.tcpdump 2>&1 |  grep m3u8  )
                         sleep 1;
 
               done 
@@ -18,7 +18,7 @@ sudo killall -9 tcpdump  &>/dev/null
 
       echo -e "All aviable host:"
       echo "######################################################################################################"
-      tcpdump -A -nnr file.tcpdump '(((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)'   2>&1 \
+      tcpdump -A -nnr $file.tcpdump '(((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)'   2>&1 \
       | egrep --line-buffered "^........(GET |HTTP\/|POST \  |HEAD )|^[A-Za-z0-9-]+: "\
       | sed -r 's/^........(GET |HTTP\/|POST |HEAD )/\n\1/g' | grep Host |  awk '{print $2}'  
       echo -e "######################################################################################################\n"
@@ -27,7 +27,7 @@ sudo killall -9 tcpdump  &>/dev/null
 
 #############################################################################################################################################################
      
-     host=$(tcpdump -A -nnr file.tcpdump '(((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)'  2>&1  \
+     host=$(tcpdump -A -nnr $file.tcpdump '(((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)'  2>&1  \
      | egrep --line-buffered "^........(GET |HTTP\/|POST \  |HEAD )|^[A-Za-z0-9-]+: " \
      | sed -r 's/^........(GET |HTTP\/|POST |HEAD )/\n\1/g' | grep Host | awk '{print $2}'\
      | sed -r '/^[a-z]/d' | head -n 1)
@@ -36,19 +36,19 @@ sudo killall -9 tcpdump  &>/dev/null
 
               for i in "${array[@]}" ;do
 
-#     host=$(tcpdump -nnr file.tcpdump | grep $i | awk ' {print $5} ' | sed 's/.$//'   | rev | sed -e 's/\([0-9]\)\./\1:/' | rev  )
-      GET=$(tcpdump -nnr file.tcpdump   2>&1 | grep $i | awk ' {print $24} ')
+#     host=$(tcpdump -nnr $file.tcpdump | grep $i | awk ' {print $5} ' | sed 's/.$//'   | rev | sed -e 's/\([0-9]\)\./\1:/' | rev  )
+      GET=$(tcpdump -nnr $file.tcpdump   2>&1 | grep $i | awk ' {print $24} ')
   
 #                  if [ -z "$GET" ] ;then 
 #  
-#                     GET=$(tcpdump -A  -nnr file.tcpdump  2>&1  | grep $i | awk ' {print $2} ')
+#                     GET=$(tcpdump -A  -nnr $file.tcpdump  2>&1  | grep $i | awk ' {print $2} ')
 #
 #                  fi
  
 
                  while [ -z "$GET" ] ;do  
         
-             GET=$(tcpdump -A -nnr file.tcpdump   2>&1 | grep $i  | awk '{for (i=2;i<=4;i++)print $i}' | sed -n '/^.\{15\}/p')
+             GET=$(tcpdump -A -nnr $file.tcpdump   2>&1 | grep $i  | awk '{for (i=2;i<=4;i++)print $i}' | sed -n '/^.\{15\}/p')
             
                   done 
 
