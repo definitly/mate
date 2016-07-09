af671efe47e78e633b70261af23ef77dae65d1c9
Your Name <you@example.com>
2016-07-09 10:35:11 +0300

    новый файл:    userfolder/scripts/tcpdump_flv.sh

---
+++ userfolder/scripts/tcpdump_flv.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+filetcpdump=/tmp/1.tcpdump
+doas rm $filetcpdump
+doas tcpdump -w $filetcpdump -s 0 -i ale0 &
+
+              while   [ -z "$rt" ]  ; do
+
+                        rt=$(  tcpdump  -A -nnr $filetcpdump 2>&1 |  grep flv  )
+                        sleep 1;
+
+              done 
+ 
+doas killall -9 tcpdump  &>/dev/null 
+
+##########################################################################################################################################################
+# all aviable hosts 
+
+      echo -e "All aviable host:"
+      echo "######################################################################################################"
+      tcpdump -A -nnr $filetcpdump '(((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)'   2>&1 \
+      | egrep --line-buffered "^........(GET |HTTP\/|POST \  |HEAD )|^[A-Za-z0-9-]+: "\
+      | sed -r 's/^........(GET |HTTP\/|POST |HEAD )/\n\1/g' | grep Host |  awk '{print $2}'  
+      echo -e "######################################################################################################\n"
+     
+
+
+#############################################################################################################################################################
+     
+     host=$(tcpdump -A -nnr $filetcpdump '(((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)'  2>&1  \
+     | egrep --line-buffered "^........(GET |HTTP\/|POST \  |HEAD )|^[A-Za-z0-9-]+: " \
+     | sed -r 's/^........(GET |HTTP\/|POST |HEAD )/\n\1/g' | grep Host | awk '{print $2}'\
+     | sed -r '/^[a-z]/d' | head -n 1)
+
+      array1="flv" 
+      array2="mp4" 
+
+              for i in $(seq 1 2) ;do
+
+#     host=$(tcpdump -nnr $filetcpdump | grep $i | awk ' {print $5} ' | sed 's/.$//'   | rev | sed -e 's/\([0-9]\)\./\1:/' | rev  )
+      GET=$(tcpdump -nnr $filetcpdump   2>&1 | eval grep "\$array$i" | awk ' {print $24} ')
+  
+#                  if [ -z "$GET" ] ;then 
+#  
+#                     GET=$(tcpdump -A  -nnr $filetcpdump  2>&1  | grep $i | awk ' {print $2} ')
+#
+#                  fi
+ 
+
+                 while [ -z "$GET" ] ;do  
+        
+             GET=$(tcpdump -A -nnr $filetcpdump   2>&1 |  eval grep "\$array$i" | awk '{for (i=2;i<=4;i++)print $i}' | sed -n '/^.\{15\}/p')
+            
+                  done 
+
+url=http://$host$GET
+echo $host
+echo -e "$GET\n"
+echo -e "######################################################################################################\n"
+echo -e "$url\n" 
+
+mpv --no-resume-playback $url  > /dev/null 2>&1
+
+             done
+
+
+
