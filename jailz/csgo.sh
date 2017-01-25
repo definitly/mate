89d19bc2b50d25a307413b940bf11b5abb11e1ba
Your Name <you@example.com>
2017-01-25 19:06:10 +0300

    новый файл:    jailz/csgo.sh

---
+++ jailz/csgo.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd "/home/definitly/.steam/steam/steamapps/common/Counter-Strike Global Offensive"
+export   LD_LIBRARY_PATH="/home/definitly/.steam/steam/steamapps/common/Counter-Strike Global Offensive/bin/linux64"
+./csgo_linux64
