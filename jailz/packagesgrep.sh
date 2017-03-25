bcdb88010b9d55c16b8921a668dfc5188e87e42c
Your Name <you@example.com>
2017-03-25 10:22:35 +0300

    изменено:      jailz/packagesgrep.sh

--- jailz/packagesgrep.sh
+++ jailz/packagesgrep.sh
@@ -8,7 +8,8 @@ arch=amd64
           packages=$(echo $i | sed "s/_.*//" |  rev | sed -r 's/\/.+//' | rev)
 
   href=$(curl   http://packages.ubuntu.com/yakkety/$arch/$packages/download | grep "de.archive.ubuntu.com" | grep -Eo 'href="[^"]*' | cut -d\" -f2- | tail -n 1 | sed 's,http://de.archive.ubuntu.com/ubuntu/,,g')
-
+  href2=$(curl   http://packages.ubuntu.com/yakkety/$arch/$packages/download | grep "security.ubuntu.com" | grep -Eo 'href="[^"]*' | cut -d\" -f2- | tail -n 1 | sed 's,http://security.ubuntu.com/ubuntu/,,g')
 echo $href >> listpackages_16.10_x86_64
+echo $href2 >> listpackages_16.10_x86_64
   
              done
