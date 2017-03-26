25f6b85af1d658958a8399f81e3f7b10825cdaa1
Your Name <you@example.com>
2017-03-26 11:06:44 +0300

    изменено:      jailz/packagesgrep.sh

--- jailz/packagesgrep.sh
+++ jailz/packagesgrep.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
-
-
+#precise 12.04
+#trusty  14.04
+#xenial  16.04
+#zesty   17.04
 #arch=i386
 arch=amd64
-ubuntuversion=yakkety
-  for i in $(cat listpackages_14.04_x86_64); do
+ubuntuversion=zesty
+UBUNTU=17.04
+
+
+
+  for i in $(cat listpackages_14.04); do
           packages=$(echo $i | sed "s/_.*//" |  rev | sed -r 's/\/.+//' | rev)
 
   href=$(curl   http://packages.ubuntu.com/$ubuntuversion/$arch/$packages/download | grep "de.archive.ubuntu.com" | grep -Eo 'href="[^"]*' | cut -d\" -f2- | tail -n 1 | sed 's,http://de.archive.ubuntu.com/ubuntu/,,g')
   href2=$(curl   http://packages.ubuntu.com/$ubuntuversion/$arch/$packages/download | grep "security.ubuntu.com" | grep -Eo 'href="[^"]*' | cut -d\" -f2- | tail -n 1 | sed 's,http://security.ubuntu.com/ubuntu/,,g')
-echo $href >> 1
-echo $href2 >> 1
   
+   if [ "$arch" == "i386" ]; then
+ 
+
+  echo $href >>  listpackages_$UBUNTU
+  echo $href2 >> listpackages_$UBUNTU
+  
+   fi 
+
+    echo $href >>  listpackages_$UBUNTU"_x86_64"
+    echo $href2 >> listpackages_$UBUNTU"_x86_64"
+
              done
