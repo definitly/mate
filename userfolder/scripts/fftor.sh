f0c8ba45d65fec6be5623c1fd3b29ed23c283c75
Your Name <you@example.com>
2017-09-16 06:51:44 +0300

    изменено:      userfolder/scripts/fftor.sh

--- userfolder/scripts/fftor.sh
+++ userfolder/scripts/fftor.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-doas  fetch  "https://www.dropbox.com/s/r096szusbkvwrq6/mozilla.tar.gz?dl=1"
+doas  fetch  "https://www.dropbox.com/s/991wpmjglo07w1w/mozilla.tar.gz?dl=1"
 doas tar -zxvf mozilla.tar.gz?dl=1 -C  /home/ff
 doas -u ff nspluginwrapper -a -i
 doas rm mozilla.tar.gz?dl=1
