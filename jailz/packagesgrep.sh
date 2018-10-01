563388283311ecf9682efe52bbad7a337d7f3c23
Your Name <you@example.com>
2018-10-01 19:06:23 +0300

    изменено:      jailz/packagesgrep.sh 	изменено:      userfolder/.config/openbox/menu.xml

--- jailz/packagesgrep.sh
+++ jailz/packagesgrep.sh
@@ -3,14 +3,14 @@
 #trusty  14.04
 #xenial  16.04
 
-#arch=i386
-arch=amd64
-ubuntuversion=artfu
-UBUNTU=17.04
+arch=i386
+#arch=amd64
+ubuntuversion=trusty
+UBUNTU=14.04
 
 
 
-  for i in $(cat listpackages_14.04); do
+  for i in $(cat listpackages_17.04); do
           packages=$(echo $i | sed "s/_.*//" |  rev | sed -r 's/\/.+//' | rev)
 
   href=$(curl   https://packages.ubuntu.com/$ubuntuversion/$arch/$packages/download | grep "de.archive.ubuntu.com" | grep -Eo 'href="[^"]*' | cut -d\" -f2- | tail -n 1 | sed 's,http://de.archive.ubuntu.com/ubuntu/,,g')
